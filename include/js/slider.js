ae3cac067d1a24ae481333eb0362b923ba001ba9
Somi <somy.taheri@gmail.com>
2014-12-19 09:05:55 +1100

    edited. slider finished

--- include/js/slider.js
+++ include/js/slider.js
@@ -2,20 +2,26 @@
  * Created by somi on 15/12/14.
  */
 //jQuery(document).ready(function ($) {
-    var currentId;
-    $('.imgModal').click(function startingImage() {
-            currentId = Number($(this).attr('id'));
-            console.log(currentId);
+var currentId =9;
+var changedOpts = $('.cycle-streams').data('cycle.startingSlide');
 
-            return currentId;
+function heshmsat(id){
+    $('.cycle-slideshow').before('<ul id="thumbnail">').cycle(
 
-        }
+        changedOpts = id,
+        changedOpts.speed = 10
+
     );
+}
 
-$('.cycle-slideshow').before('<ul id="thumbnail">').cycle({
-        startingSlide: startingImage(), // zero-based
+$('.imgModal').click(function startingImage() {
 
-    }
+    currentId = Number($(this).attr('id'));
 
-);
+    heshmsat(currentId);
+
+});
+
+
+
 
