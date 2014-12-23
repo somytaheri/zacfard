fa09005ded60cdd48861bb3a2d37732939ec724c
Somi <somy.taheri@gmail.com>
2014-12-23 13:59:13 +1100

    padding changed. logo fixed

--- include/js/scroll.js
+++ include/js/scroll.js
@@ -1,7 +1,16 @@
 var $root = $('html, body'),
     headerHeight = $("#header").css("height").replace('px', '');
-$(".navbar-collapse ul li a, .navbar-brand").click(function() {
+$(".navbar-collapse ul li a").click(function() {
     $('.navbar-toggle:visible').click();
+    var href = $.attr(this, 'href');
+    $root.animate({
+        scrollTop: $(href).offset().top - headerHeight
+    }, 700, function () {
+        window.location.hash = href;
+    });
+    return false;
+});
+$(".navbar-brand").click(function() {
     var href = $.attr(this, 'href');
     $root.animate({
         scrollTop: $(href).offset().top - headerHeight
